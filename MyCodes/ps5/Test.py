ac58dbf7c25ae032b65c3dfa4912c1865964d0f8
Chaeguevara <dfso2@naver.com>
2021-05-08 20:00:14 +0900

    프로젝트 종료
     더이상 진척이 없기에 종료함

---
+++ MyCodes/ps5/Test.py
@@ -0,0 +1,22 @@
+import string
+
+Ok_1 = 'PURPLE COW'
+Ok_2 = 'The purple cow is soft and cuddly.'
+Ok_3 = 'The farmer owns a really PURPLE cow.'
+Ok_4 = 'Purple!!! Cow!!!'
+Ok_5 = 'purple@#$%cow'
+Ok_5 = 'Did you see a purple cow?'
+
+No_1 = 'Purple cows are cool!'
+No_2 = 'The purple blob over there is a cow.'
+No_3 = 'How now brown cow.'
+No_4 = 'Cow!!! Purple!!!'
+No_5 = 'purplecowpurplecowpurplecow'
+
+punctuation_string = string.punctuation
+punctuation_string += " "
+print(Ok_2.lower())
+
+# WO punctuation
+
+# With Punctuation
